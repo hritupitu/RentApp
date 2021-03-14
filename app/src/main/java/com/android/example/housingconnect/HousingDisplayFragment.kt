24a91fa5ef712cb062d7f14e91abab3a7d016259
hritvik <hritvik.agarwal@gmail.com>
2021-03-13 17:38:31 -0800

    Type fix

--- app/src/main/java/com/android/example/housingconnect/HousingDisplayFragment.kt
+++ app/src/main/java/com/android/example/housingconnect/HousingDisplayFragment.kt
@@ -41,6 +41,7 @@ class HousingDisplayFragment : Fragment() {
         datePosted.text=currentPost.date
         description.text = currentPost.desc
         movein.text = currentPost.moveIn
+        type.text = currentPost.type
 
         // TODO: PHASE 3.3 - Create an onclick listener on a button to send an email.
         //  this stage will require sending an implicit intent to an email application.
